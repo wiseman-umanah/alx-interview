0d63423fbeed6f5bbbabe7a79ed0c115cd785e88
wiseman-umanah <wisemanumanah@gmail.com>
2024-06-20 16:34:51 +0100

    Added a new codebase entirely while commenting out the previous one, but work the same way But the later passes the alx checks

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,7 +5,7 @@ Printing the stats after each 10 lines
 import sys
 import re
 
-# Dictionary to keep track of the count of each status code
+'''# Dictionary to keep track of the count of each status code
 stats = {"200": 0, "301": 0,
          "400": 0, "401": 0,
          "403": 0, "404": 0,
@@ -41,10 +41,11 @@ def process_log_line(line):
             status_code, file_size = match[-1].split()
             if status_code in stats:
                 stats[status_code] += 1
-            #temporary to cause error for non int so that file_size is not added.
+            """temporary to cause error for non int so
+            that file_size is not added."""
             int(status_code)
             return int(file_size)
-    except:
+    except ValueError:
         return 0
 
 
@@ -76,3 +77,39 @@ def main():
 
 if __name__ == "__main__":
     main()
+'''
+
+counter = 0
+file_size = 0
+statusC_counter = {200: 0, 301: 0, 400: 0,
+                   401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+
+def printCodes(dict, file_s):
+    """Prints the status code and the number of times they appear"""
+    print("File size: {}".format(file_s))
+    for key in sorted(dict.keys()):
+        if statusC_counter[key] != 0:
+            print("{}: {}".format(key, dict[key]))
+
+
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            split_string = re.split('- |"|"| " " ', str(line))
+            statusC_and_file_s = split_string[-1]
+            if counter != 0 and counter % 10 == 0:
+                printCodes(statusC_counter, file_size)
+            counter = counter + 1
+            try:
+                statusC = int(statusC_and_file_s.split()[0])
+                f_size = int(statusC_and_file_s.split()[1])
+                if statusC in statusC_counter:
+                    statusC_counter[statusC] += 1
+                file_size = file_size + f_size
+            except ValueError:
+                pass
+        printCodes(statusC_counter, file_size)
+    except KeyboardInterrupt:
+        printCodes(statusC_counter, file_size)
+        raise

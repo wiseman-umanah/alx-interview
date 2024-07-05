6692532d22efe04e2b2086c5594e7221aeec2dcc
wiseman-umanah <wisemanumanah@gmail.com>
2024-07-05 10:16:19 +0100

    fix typo error

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -35,7 +35,7 @@ def main() -> None:
     if sys.argv[1].isnumeric():
         n = int(sys.argv[1])
         if n < 4:
-            print("N must be a least 4")
+            print("N must be at least 4")
             exit(1)
         else:
             board = []

0f12bea3ae99217c642bf91c23f89ff65c465e00
wiseman-umanah <wisemanumanah@gmail.com>
2024-08-09 05:30:02 +0100

    Started and completed the prime game task

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""The Prime Game Module
+"""
+
+
+def is_prime(num: int) -> bool:
+    """check for prime number validity
+
+    Args:
+        num (int): number to validate
+
+    Returns:
+        bool: true or false
+    """
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def isWinner(x, nums):
+    """Finds Winner of x rounds
+
+    Args:
+        x (int): the number of rounds
+        nums (list): list of numbers
+
+    Returns:
+        str: Maria or Ben depending on who wins
+    """
+    if not nums or x < 1:
+        return None
+
+    maria, ben = 0, 0
+
+    for n in nums:
+        primes = [i for i in range(2, n + 1) if is_prime(i)]
+        move_count = 0
+
+        for i in primes:
+            # prime = primes.pop(0)
+            # primes = [p for p in primes if p % prime != 0]
+            # print(primes)
+            move_count += 1
+
+        if move_count % 2 == 0:
+            ben += 1
+        else:
+            maria += 1
+
+    return "Maria" if maria > ben else "Ben"
---
+++ 0x0A-primegame/README.md
@@ -0,0 +1,6 @@
+### PRIME GAME
+The Prime Game is a dynamic and engaging game for Maria and Ben. In each round, the players decide who wins by taking prime numbers and their multiples from 0 up to a list of numbers.
+
+The player with the highest number of wins across all rounds is declared the overall winner.
+
+This task is designed to enhance dynamic programming skills, cognitive reasoning, and Python list manipulation.
